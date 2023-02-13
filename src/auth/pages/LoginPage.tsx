3328c621cc295dbedd7c34c91c7552f0898c990b
Alberto Morán <a.moran.cuenca@gmail.com>
2023-02-13 05:50:56 +0100

    feat: create auth layout

---
+++ src/auth/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import AuthLayout from "../layouts/AuthLayout"
+
+const LoginPage = () => {
+  return (
+    <AuthLayout>
+
+    </AuthLayout>
+    
+  )
+}
+
+export default LoginPage
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './router/AppRouter'
+import "./styles/config.css"
+import "./styles/normalize.css"
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
