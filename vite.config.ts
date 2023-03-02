7b3f40c48665eaa94655f683296081d2963f3eaa
Alberto Morán <a.moran.cuenca@gmail.com>
2023-03-02 07:26:43 +0100

    add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
   server: {
     watch: {
       usePolling: true,
@@ -10,5 +14,5 @@ export default defineConfig({
     host: true,
     strictPort: true,
     port: 5173,
-  }
+  },
 })
