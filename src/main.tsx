91577ce774f049ec25af31d3e4dc44de6eadbe59
Alberto Morán <a.moran.cuenca@gmail.com>
2023-03-02 07:17:41 +0100

    delete

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import AppRouter from './router/AppRouter'
-import "./styles/config.css"
-import "./styles/normalize.css"
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <AppRouter />
+    <h1>HELLO</h1>
   </BrowserRouter>,
 )
